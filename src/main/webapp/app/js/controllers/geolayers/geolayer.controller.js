c47e8e6519a33ab4cc12cfa789ef36234beb28d3
Hasdai Pacheco <haxdai@gmail.com>
2017-04-27 14:29:00 -0500

    Added resourceURL link display

--- src/main/webapp/app/js/controllers/geolayers/geolayer.controller.js
+++ src/main/webapp/app/js/controllers/geolayers/geolayer.controller.js
@@ -5,16 +5,22 @@
     .module("FST2015PM.controllers")
     .controller("GeolayerCtrl", GeolayerCtrl);
 
-    GeolayerCtrl.$inject = ["$GeoLayer"];
-    function GeolayerCtrl($GeoLayer) {
+    GeolayerCtrl.$inject = ["$GeoLayer", "$timeout"];
+    function GeolayerCtrl($GeoLayer, $timeout) {
       let cnt = this;
       cnt.layerList = [];
 
       $GeoLayer.listGeoLayers()
-      .then(res => {
-        console.log(res);
+      .then(function(res) {
         if (res.data && res.data.length) {
           cnt.layerList = res.data;
+
+          $timeout(function() {
+            $('a[rel=popover]').popover({
+              html: 'true',
+              placement: 'left'
+            });
+          });
         }
       });
 
