2f55aac28061d1bfd6ca61598894016c14049ded
Hasdai Pacheco <me@hasdaipacheco.com>
2018-09-16 22:16:42 -0500

    Code refactor for extractor controllers

--- src/main/webapp/app/js/controllers/extractors/extractor.controller.js
+++ src/main/webapp/app/js/controllers/extractors/extractor.controller.js
@@ -1,96 +1,95 @@
 (function() {
-  'use strict'
+    'use strict';
 
-  angular
-    .module("FST2015PM.controllers")
-    .controller("ExtractorCtrl", ExtractorCtrl);
+    angular
+        .module("FST2015PM.controllers")
+        .controller("ExtractorCtrl", ExtractorCtrl);
 
-  ExtractorCtrl.$inject = ["$Datasource", "$interval", "$Extractor", "$scope", "toaster"];
-  function ExtractorCtrl($Datasource, $interval, $Extractor, $scope, toaster) {
-    let cnt = this;
-    let extractorsLoaded = false;
-    cnt.interval = undefined,
-
-    cnt.extractorList = [];
-
-    $Datasource.listObjects("Extractor")
-    .then(function(res) {
-      if (res.data.data && res.data.data.length) {
-        cnt.extractorList = res.data.data.map(function(item) {
-          item.status = "DESCONOCIDO";
-
-          return item;
-        });
-        extractorsLoaded = true;
-
-        cnt.interval = $interval(function () {
-          if (extractorsLoaded) {
-            cnt.extractorList.forEach(function(item) {
-              $Extractor.getStatus(item._id)
-              .then(function(res) {
-                item.status = res;
-              })
-              .catch(function(error) {
-                item.status = "DESCONOCIDO";
-              });
-            });
-          }
-        }, 3000);
-      }
-    });
-
-    cnt.startExtractor = function (extractor) {
-      $Extractor.startExtractor(extractor._id);
-      var msg = "Se ha iniciado el extractor "+extractor.name;
-      toaster.pop({
-        type: 'success',
-        body: msg,
-        showCloseButton: true,
-      });
-    };
-
-    cnt.canStart = function(extractor) {
-      return !extractor.periodic && extractor.status && (extractor.status === "LOADED" || extractor.status === "STARTED");
-    };
-
-    cnt.deleteExtractor = function(id) {
-      bootbox.confirm({
-        message: "<h4>Este extractor será eliminado permanentemente. \n ¿Desea continuar?</h4>",
-        buttons: {
-          cancel: {
-            label: "Cancelar"
-          },
-          confirm: {
-            label: "Aceptar"
-          }
-        },
-        callback: function(result) {
-          if (result) {
-            $Datasource.removeObject(id, "Extractor")
-            .then(function(res) {
-              cnt.extractorList.filter(function(elem, i) {
-                if (elem._id === id) {
-                  cnt.extractorList.splice(i, 1);
-                }
-              });
-            });
-            toaster.pop({
-              type: 'success',
-              body: 'Se ha eliminado el extractor',
-              showCloseButton: true,
-            });
-          }
-        }
-      });
-    };
-
-    $scope.$on('$destroy', () => {
-      if (angular.isDefined(cnt.interval)) {
-        $interval.cancel(cnt.interval);
+    ExtractorCtrl.$inject = ["$Datasource", "$interval", "$Extractor", "$scope", "toaster"];
+    function ExtractorCtrl($Datasource, $interval, $Extractor, $scope, toaster) {
+        var cnt = this;
+        var extractorsLoaded = false;
         cnt.interval = undefined;
-      }
-    });
+        cnt.extractorList = [];
 
-  }
+        $Datasource.listObjects("Extractor")
+            .then(function(res) {
+                if (res.data.data && res.data.data.length) {
+                    cnt.extractorList = res.data.data.map(function(item) {
+                        item.status = "DESCONOCIDO";
+
+                        return item;
+                    });
+                    extractorsLoaded = true;
+
+                    cnt.interval = $interval(function () {
+                        if (extractorsLoaded) {
+                            cnt.extractorList.forEach(function(item) {
+                                $Extractor.getStatus(item._id)
+                                    .then(function(res) {
+                                        item.status = res;
+                                    })
+                                    .catch(function(error) {
+                                        item.status = "DESCONOCIDO";
+                                    });
+                            });
+                        }
+                    }, 3000);
+                }
+            });
+
+        cnt.startExtractor = function (extractor) {
+            $Extractor.startExtractor(extractor._id);
+            var msg = "Se ha iniciado el extractor "+extractor.name;
+            toaster.pop({
+                type: 'success',
+                body: msg,
+                showCloseButton: true
+            });
+        };
+
+        cnt.canStart = function(extractor) {
+            return !extractor.periodic && extractor.status && (extractor.status === "LOADED" || extractor.status === "STARTED");
+        };
+
+        cnt.deleteExtractor = function(id) {
+            bootbox.confirm({
+                message: "<h4>Este extractor será eliminado permanentemente. \n ¿Desea continuar?</h4>",
+                buttons: {
+                    cancel: {
+                        label: "Cancelar"
+                    },
+                    confirm: {
+                        label: "Aceptar"
+                    }
+                },
+                callback: function(result) {
+                    if (result) {
+                        $Datasource.removeObject(id, "Extractor")
+                            .then(function(res) {
+                                cnt.extractorList.filter(function(elem, i) {
+                                    if (elem._id === id) {
+                                        cnt.extractorList.splice(i, 1);
+                                    }
+                                });
+                            });
+                        toaster.pop({
+                            type: 'success',
+                            body: 'Se ha eliminado el extractor',
+                            showCloseButton: true
+                        });
+                    }
+                }
+            });
+        };
+
+        $scope.$on('$destroy', function() {
+            if (angular.isDefined(cnt.interval)) {
+                $interval.cancel(cnt.interval);
+                cnt.interval = undefined;
+            }
+        });
+
+    }
 
 })();
--- src/main/webapp/app/js/controllers/extractors/extractoredit.controller.js
+++ src/main/webapp/app/js/controllers/extractors/extractoredit.controller.js
@@ -1,122 +1,122 @@
 (function() {
-  'use strict'
+    'use strict';
 
-  angular
-    .module("FST2015PM.controllers")
-    .controller("ExtractorEditCtrl", ExtractorEditCtrl);
+    angular
+        .module("FST2015PM.controllers")
+        .controller("ExtractorEditCtrl", ExtractorEditCtrl);
 
-  ExtractorEditCtrl.$inject = ["$Datasource", "$stateParams", "$state", "$Extractor", "$http", "toaster"];
-  function ExtractorEditCtrl($Datasource, $stateParams, $state, $Extractor, $http, toaster) {
-    let cnt = this;
-    cnt.formTitle = "Agregar extractor";
-    cnt.extractorData = {};
-    cnt.dsList = [];
-    cnt.charsetList = [];
-    cnt.processing = false;
+    ExtractorEditCtrl.$inject = ["$Datasource", "$stateParams", "$state", "$Extractor", "$http", "toaster"];
+    function ExtractorEditCtrl($Datasource, $stateParams, $state, $Extractor, $http, toaster) {
+        var cnt = this;
+        cnt.formTitle = "Agregar extractor";
+        cnt.extractorData = {};
+        cnt.dsList = [];
+        cnt.charsetList = [];
+        cnt.processing = false;
 
-    if ($stateParams.extractordef) {
-      cnt.formTitle = "Previsualización";
-      cnt.extractorData = $stateParams.extractordef;
-      $Extractor.downloadPreview(cnt.extractorData.fileLocation, cnt.extractorData.zipped, cnt.extractorData.charset, cnt.extractorData.zipPath)
-      .then(function(res) {
-        if (res.data && res.data.columns && res.data.data) {
-          dataviz.dataTablesFactory.createDataTable("dataPreview", {
-            scrollX: true,
-            scrollY: "300px",
-            scrollCollapse: true,
-            ordering: false,
-            searching: false,
-            paging: false,
-            processing: true,
-            info: false,
-            columns: res.data.columns,
-            data:res.data.data
-          });
-        }
-      }).catch(function(err) {
-        console.log(err);
-      });
-    } else {
-      $Extractor.getEncodingList()
-      .then(function(res) {
-        cnt.charsetList = res.map(function(item) {
-          var type = "Todos";
-          if (item === "ISO-8859-1" || item === "UTF-8") type = "Más usados";
-          return {
-            type: type,
-            name: item
-          };
-        });
-      });
-
-      $Datasource.listDatasources()
-      .then(function(res) {
-        if (res.data && res.data.length) {
-          cnt.dsList = res.data;
-          cnt.dsList = cnt.dsList.map(function(item) { return {id: item.name, name: item.name} });
-
-          $Datasource.listObjects("Extractor").then(function(res) {
-            if(res.data.data && res.data.data.length) {
-              res.data.data.forEach(function(item) {
-                if (item._id !== cnt.extractorData._id) {
-                  let idx = cnt.dsList.map(function(item) { return item.name; }).indexOf(item.dataSource);
-                  if (idx > -1) {
-                    cnt.dsList.splice(idx, 1);
-                  }
-                }
-              });
-            }
-          });
-        }
-      });
-    }
-
-    if($stateParams.id && $stateParams.id.length) {
-      cnt.formTitle = "Editar extractor";
-      $Datasource.getObject($stateParams.id, "Extractor").then(function(ds) {
-        cnt.extractorData = ds.data;
-      });
-    }
-
-    cnt.previewData = function(form, objData) {
-      if (form.$valid) {
-        $state.go('admin.previewextractor', {extractordef: objData});
-      }
-    };
-
-    cnt.save = function(form) {
-      let valid = true;
-      if (form) valid = form.$valid;
-
-      if (valid) {
-        cnt.processing = true;
-        if (!cnt.extractorData._id) {
-          $Datasource.addObject(cnt.extractorData, "Extractor")
-          .then(function(response) {
-            if (response.data.data && response.data.data._id) {
-              $Extractor.loadExtractor(response.data.data._id);
-            }
-            toaster.pop({
-              type: 'success',
-              body: 'Se ha agregado el nuevo extractor',
-              showCloseButton: true,
+        if ($stateParams.extractordef) {
+            cnt.formTitle = "Previsualización";
+            cnt.extractorData = $stateParams.extractordef;
+            $Extractor.downloadPreview(cnt.extractorData.fileLocation, cnt.extractorData.zipped, cnt.extractorData.charset, cnt.extractorData.zipPath)
+                .then(function(res) {
+                    if (res.data && res.data.columns && res.data.data) {
+                        dataviz.dataTablesFactory.createDataTable("dataPreview", {
+                            scrollX: true,
+                            scrollY: "300px",
+                            scrollCollapse: true,
+                            ordering: false,
+                            searching: false,
+                            paging: false,
+                            processing: true,
+                            info: false,
+                            columns: res.data.columns,
+                            data:res.data.data
+                        });
+                    }
+                }).catch(function(err) {
+                console.log(err);
             });
-            $state.go('admin.extractors', {});
-          })
         } else {
-          $Datasource.updateObject(cnt.extractorData, "Extractor")
-          .then(function(response) {
-            $Extractor.loadExtractor(cnt.extractorData._id);
-            toaster.pop({
-              type: 'success',
-              body: 'Se ha actualizado el extractor',
-              showCloseButton: true,
-            });
-            $state.go('admin.extractors', {});
-          })
+            $Extractor.getEncodingList()
+                .then(function(res) {
+                    cnt.charsetList = res.map(function(item) {
+                        var type = "Todos";
+                        if (item === "ISO-8859-1" || item === "UTF-8") type = "Más usados";
+                        return {
+                            type: type,
+                            name: item
+                        };
+                    });
+                });
+
+            $Datasource.listDatasources()
+                .then(function(res) {
+                    if (res.data && res.data.length) {
+                        cnt.dsList = res.data;
+                        cnt.dsList = cnt.dsList.map(function(item) { return {id: item.name, name: item.name} });
+
+                        $Datasource.listObjects("Extractor").then(function(res) {
+                            if(res.data.data && res.data.data.length) {
+                                res.data.data.forEach(function(item) {
+                                    if (item._id !== cnt.extractorData._id) {
+                                        var idx = cnt.dsList.map(function(item) { return item.name; }).indexOf(item.dataSource);
+                                        if (idx > -1) {
+                                            cnt.dsList.splice(idx, 1);
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
         }
-      }
-    };
-  }
+
+        if($stateParams.id && $stateParams.id.length) {
+            cnt.formTitle = "Editar extractor";
+            $Datasource.getObject($stateParams.id, "Extractor").then(function(ds) {
+                cnt.extractorData = ds.data;
+            });
+        }
+
+        cnt.previewData = function(form, objData) {
+            if (form.$valid) {
+                $state.go('admin.previewextractor', {extractordef: objData});
+            }
+        };
+
+        cnt.save = function(form) {
+            var valid = true;
+            if (form) valid = form.$valid;
+
+            if (valid) {
+                cnt.processing = true;
+                if (!cnt.extractorData._id) {
+                    $Datasource.addObject(cnt.extractorData, "Extractor")
+                        .then(function(response) {
+                            if (response.data.data && response.data.data._id) {
+                                $Extractor.loadExtractor(response.data.data._id);
+                            }
+                            toaster.pop({
+                                type: 'success',
+                                body: 'Se ha agregado el nuevo extractor',
+                                showCloseButton: true
+                            });
+                            $state.go('admin.extractors', {});
+                        })
+                } else {
+                    $Datasource.updateObject(cnt.extractorData, "Extractor")
+                        .then(function(response) {
+                            $Extractor.loadExtractor(cnt.extractorData._id);
+                            toaster.pop({
+                                type: 'success',
+                                body: 'Se ha actualizado el extractor',
+                                showCloseButton: true
+                            });
+                            $state.go('admin.extractors', {});
+                        })
+                }
+            }
+        };
+    }
 
 })();
